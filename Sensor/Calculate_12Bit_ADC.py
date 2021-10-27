320d8fefddcc9efcec1b2fa8e97d72c7b086d88a
TexNevada <15308761+texnevada@users.noreply.github.com>
2021-10-27 21:58:50 +0200

    Cleaned up the code

--- Sensor/Calculate_12Bit_ADC.py
+++ Sensor/Calculate_12Bit_ADC.py
@@ -2,11 +2,17 @@
 def calculate(value):
     # 2048 = 0C
     # 3000 = 23C
+    # By dividing 3000-2048 / 23. You get 41.39 for each Celsius change
     # 41.39 = 1C change
+    # 41.39130434782609
+    val1 = 0
+    val2 = 0
     if int(value) >= 2048:
-        math = round(float((value-2048)/41.39130434782609), 2)
-    elif int(value) < 2048:
-        math = round(float((2048-value) / 41.39130434782609), 2)
+        val1 = value
+        val2 = 2048
+    # Changing this to else. Will come back later to add if and error correction
     else:
-        math = 0
+        val1 = 2048
+        val2 = value
+    math = round(float(val1-val2) / 41.39, 2)
     return math
