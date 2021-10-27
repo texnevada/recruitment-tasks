53629e6c88078e27fa85ecb6895d0db96d5d1388
TexNevada <15308761+texnevada@users.noreply.github.com>
2021-10-27 21:50:21 +0200

    Reads random line from temperature

--- Sensor/Read_Temperature.py
+++ Sensor/Read_Temperature.py
@@ -1,6 +1,9 @@
 # Since there is no "real" temperature sensor. It makes sense to read values from random lines of the txt file
 import random
 
-def read():
 
-    return
+def read():
+    with open("./temperature-sensor/temperature.txt") as sensor:
+        for value in sensor:
+            if random.randrange(10) == 8:
+                return value
