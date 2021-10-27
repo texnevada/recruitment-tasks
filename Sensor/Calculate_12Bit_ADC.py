2d2897734a80556b1ede936e7d81813ca7590cdd
TexNevada <15308761+texnevada@users.noreply.github.com>
2021-10-27 23:04:17 +0200

    Added kelvin, fahrenheit and configparser temp change

--- Sensor/Calculate_12Bit_ADC.py
+++ Sensor/Calculate_12Bit_ADC.py
@@ -1,18 +1,31 @@
+import configparser
+
 
 def calculate(value):
+    config = configparser.ConfigParser()
+    config.read("./config.ini")
     # 2048 = 0C
     # 3000 = 23C
     # By dividing 3000-2048 / 23. You get 41.39 for each Celsius change
     # 41.39 = 1C change
     # 41.39130434782609
-    val1 = 0
-    val2 = 0
     if int(value) >= 2048:
         val1 = value
         val2 = 2048
-    # Changing this to else. Will come back later to add if and error correction
     else:
         val1 = 2048
         val2 = value
-    math = round(float(val1-val2) / 41.39, 2)
+    celsius = round(float(val1-val2) / 41.39, 2)
+    fahrenheit = round((celsius * 1.8) + 32, 2)
+    kelvin = round(273.15 + celsius, 2)
+    # print(f"{math} Celsius")
+    print(f"\n{fahrenheit} Fahrenheit")
+    print(f"{kelvin} Kelvin")
+    # print(f"{math} = {val1, val2}")
+    if config["Sensor"]["Temperature"] == "Fahrenheit":
+        math = fahrenheit
+    elif config["Sensor"]["Temperature"] == "Kelvin":
+        math = kelvin
+    else:
+        math = celsius
     return math
