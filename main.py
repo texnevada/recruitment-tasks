749b8fff781eaaa9a9a926eac4433d7744e2f537
TexNevada <15308761+texnevada@users.noreply.github.com>
2021-10-27 22:10:13 +0200

    Added 12Bit ADC calc to main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 # This is just to get things set up.
 #
 import Sensor.Read_Temperature as Sensor
+from Sensor.Calculate_12Bit_ADC import calculate
 
 
 def main():
     value = Sensor.read()
-    print(value)
+    temp = calculate(value)
+    print(temp)
 
 
 if __name__ == "__main__":
