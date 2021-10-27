0c25b44dd25ccb98a338a5b3004415ce783c8980
TexNevada <15308761+texnevada@users.noreply.github.com>
2021-10-27 22:20:24 +0200

    Added run timer

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #
 # This is just to get things set up.
 #
+import time
 import Sensor.Read_Temperature as Sensor
 from Sensor.Calculate_12Bit_ADC import calculate
+start = time.perf_counter()
 
 
 def main():
@@ -13,3 +15,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+    finish = time.perf_counter()
+    print(f"Finished in {round(finish-start, 6)} second(s)")
