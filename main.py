71b5b7c92ce6517ba3db112f46e9140c7d488476
TexNevada <15308761+texnevada@users.noreply.github.com>
2021-10-27 21:50:33 +0200

    Main reads sensor

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #
 # This is just to get things set up.
 #
+import Sensor.Read_Temperature as Sensor
+
 
 def main():
-    pass
+    value = Sensor.read()
+    print(value)
 
 
 if __name__ == "__main__":
