aacdab86b3fb0819f1e3c37574d940903d2cb28e
TexNevada <15308761+texnevada@users.noreply.github.com>
2021-10-27 23:03:24 +0200

    Added 100ms sleep timer

--- Sensor/Read_Temperature.py
+++ Sensor/Read_Temperature.py
@@ -1,9 +1,15 @@
 # Since there is no "real" temperature sensor. It makes sense to read values from random lines of the txt file
 import random
+import time
 
 
 def read():
+    # 100ms delay before read
+    time.sleep(0.1)
+    # Reads the temperature values
     with open("./temperature-sensor/temperature.txt") as sensor:
+        # Loops until random hits 8 to simulate a temperature reader.
         for value in sensor:
             if random.randrange(10) == 8:
+                # Returns the value as a integer instead of string
                 return int(value)
