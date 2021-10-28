7bf1695f73a9cf18e0623b5894321d1bf796800c
TexNevada <15308761+texnevada@users.noreply.github.com>
2021-10-28 20:34:41 +0200

    Can now grab timed temperature readings

--- main.py
+++ main.py
@@ -7,18 +7,30 @@ from Sensor.Calculate_12Bit_ADC import calculate
 import random
 import configparser
 start = time.perf_counter()
+config = configparser.ConfigParser()
+config.read("./config.ini")
+
+
+def temperature_details(seconds: int):
+    timeout_start = time.time()
+    temp_values = []
+    while time.time() < timeout_start + seconds:
+        value = Sensor.raw_read()
+        temp = calculate(value)
+        temp_values.append(temp)
+        print(f"{temp} {config['Sensor']['Temperature']}")
+    temp_values.sort()
+    print(temp_values)
 
 
 def main():
-    config = configparser.ConfigParser()
-    config.read("./config.ini")
     while random.randrange(30):
-        value = Sensor.read()
+        value = Sensor.raw_read()
         temp = calculate(value)
         print(f"{temp} {config['Sensor']['Temperature']}")
 
 
 if __name__ == "__main__":
-    main()
+    temperature_details(5)
     finish = time.perf_counter()
     print(f"Finished in {round(finish-start, 6)} second(s)")
