179217a4f214f02c3cad62e766c7e0d7bebd7df7
TexNevada <15308761+texnevada@users.noreply.github.com>
2021-10-28 21:02:20 +0200

    Added list and json to main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # This is just to get things set up.
 #
 import time
+from datetime import datetime
 import Sensor.Read_Temperature as Sensor
 from Sensor.Calculate_12Bit_ADC import calculate
 import random
@@ -14,6 +15,9 @@ config.read("./config.ini")
 def temperature_details(seconds: int):
     timeout_start = time.time()
     temp_values = []
+    # UTC time
+    datetime_start = datetime.utcnow().isoformat()
+
     while time.time() < timeout_start + seconds:
         value = Sensor.raw_read()
         temp = calculate(value)
@@ -21,6 +25,20 @@ def temperature_details(seconds: int):
         print(f"{temp} {config['Sensor']['Temperature']}")
     temp_values.sort()
     print(temp_values)
+    print(temp_values[0])
+    print(temp_values[-1])
+    average_temp = sum(temp_values) / len(temp_values)
+    datetime_end = datetime.utcnow().isoformat()
+    json = {
+        "time": {
+            "start": datetime_start,
+            "end": datetime_end
+        },
+        "min": temp_values[0],
+        "max": temp_values[-1],
+        "average": round(average_temp, 2)
+    }
+    print(json)
 
 
 def main():
@@ -31,6 +49,6 @@ def main():
 
 
 if __name__ == "__main__":
-    temperature_details(5)
+    temperature_details(120)
     finish = time.perf_counter()
     print(f"Finished in {round(finish-start, 6)} second(s)")
