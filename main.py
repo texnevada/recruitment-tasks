b72b666f0654962ef98974847ba1eed7584b5074
TexNevada <15308761+texnevada@users.noreply.github.com>
2021-10-28 22:23:06 +0200

    Fixed bug with assignment. Cleaned up code.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from Sensor.Calculate_12Bit_ADC import calculate
 from Sensor.HTTP_REST import post
 import random
 import configparser
-start = time.perf_counter()
+# start = time.perf_counter()
 config = configparser.ConfigParser()
 config.read("./config.ini")
 
@@ -23,23 +23,25 @@ def temperature_details(seconds: int):
         value = Sensor.raw_read()
         temp = calculate(value)
         temp_values.append(temp)
-        print(f"{temp} {config['Sensor']['Temperature']}")
+        # print(f"{temp} {config['Sensor']['Temperature']}")
     temp_values.sort()
-    print(temp_values)
-    print(temp_values[0])
-    print(temp_values[-1])
+    # print(temp_values)
+    # print(temp_values[0])
+    # print(temp_values[-1])
     average_temp = sum(temp_values) / len(temp_values)
     datetime_end = datetime.utcnow().isoformat()
     json = {
         "time": {
-            "start": datetime_start,
-            "end": datetime_end
+            "start": str(datetime_start),
+            "end": str(datetime_end)
         },
-        "min": temp_values[0],
-        "max": temp_values[-1],
-        "average": round(average_temp, 2)
+        "min": float(temp_values[0]),
+        "max": float(temp_values[-1]),
+        "avg": float(round(average_temp, 2))
     }
-    print(json)
+    # Objective says use "average": float but returns 400
+    # using "avg": float works fine...
+    print(f"Attempting to send: {json}")
     post(json)
 
 
@@ -51,6 +53,8 @@ def main():
 
 
 if __name__ == "__main__":
-    temperature_details(120)
-    finish = time.perf_counter()
-    print(f"Finished in {round(finish-start, 6)} second(s)")
+    while True:
+        start = time.perf_counter()
+        temperature_details(int(config["Backend"]["interval"]))
+        finish = time.perf_counter()
+        print(f"Finished in {round(finish-start, 6)} second(s)\n")
