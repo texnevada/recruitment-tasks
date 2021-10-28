46e92e6fc10420fca12b3d750adab89823b7e7da
TexNevada <15308761+texnevada@users.noreply.github.com>
2021-10-28 20:33:32 +0200

    renamed read_speed to sensor_read_speed. Renamed function

--- Sensor/Read_Temperature.py
+++ Sensor/Read_Temperature.py
@@ -4,12 +4,12 @@ import time
 import configparser
 
 
-def read():
+def raw_read():
     # Putting the config parser in the function allows for changes in the config without
     # having to reload the program
     config = configparser.ConfigParser()
     config.read("./config.ini")
-    time.sleep(float(config["Sensor"]["read_speed"]))
+    time.sleep(float(config["Sensor"]["sensor_read_speed"]))
     # Reads the temperature values
     with open("./temperature-sensor/temperature.txt") as sensor:
         # Loops until random hits 8 to simulate a temperature reader.
