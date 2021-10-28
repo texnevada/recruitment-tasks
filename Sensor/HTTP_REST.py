a4ce89e4292dbc5800f98c4d798c95482010489c
TexNevada <15308761+texnevada@users.noreply.github.com>
2021-10-28 22:23:46 +0200

    Fixed up fallback to send entire list

--- Sensor/HTTP_REST.py
+++ Sensor/HTTP_REST.py
@@ -12,16 +12,20 @@ def post(json: dict):
     config.read("./config.ini")
     url = config["Backend"]["url"]
     fallback_url = config["Backend"]["fallback_url"]
-    r = requests.post(url, json=json)
+    # print(url)
+    # print(fallback_url)
+    r = requests.post(url=url, json=json)
     status_code = r.status_code
-    print(status_code)
-    if status_code == "500":
+    print(f"Status code: {status_code}")
+    if str(status_code) == "500":
         if len(failed_attempts) == 10:
             del failed_attempts[-1]
         failed_attempts.append(json)
     if bool(failed_attempts):
         if failed_attempts[0] != json:
-            for entry in failed_attempts:
-                r2 = requests.post(fallback_url, json=entry)
-                if r2.status_code == "200":
-                    failed_attempts.remove(entry)
+            r2 = requests.post(url=fallback_url, json=failed_attempts)
+            print(f"Fallback status code: {r2.status_code}")
+            if str(r2.status_code) == "200":
+                failed_attempts.clear()
+
+    print(f"Failed: {failed_attempts}")
